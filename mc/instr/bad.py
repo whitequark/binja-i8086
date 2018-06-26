21b69b0f724200caf6b64f3a7ca6d3024ca16cd8
whitequark <whitequark@whitequark.org>
2018-06-26 12:01:22 +0000

    Fix unassigned opcodes to emit undefined LLOP.

--- mc/instr/bad.py
+++ mc/instr/bad.py
@@ -10,10 +10,10 @@ class Unassigned(Instruction):
     def name(self):
         return '(unassigned)'
 
+    def lift(self, il, addr):
+        il.append(il.undefined())
 
-class UnassignedRM(InstrHasModRegRM, Instruction):
+
+class UnassignedRM(InstrHasModRegRM, Unassigned):
     def length(self):
         return 2
-
-    def name(self):
-        return '(unassigned)'
