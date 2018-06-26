88c5ba81445cd63e0b0d4654412c76f599516d7b
whitequark <whitequark@whitequark.org>
2018-06-26 11:17:31 +0000

    Implement xchg reg,rm.

--- mc/instr/xchg.py
+++ mc/instr/xchg.py
@@ -5,7 +5,7 @@ from ..tables import *
 from . import *
 
 
-__all__ = ['Nop', 'Xchg']
+__all__ = ['Nop', 'XchgRegReg', 'XchgRegRM']
 
 
 class Nop(Instruction):
@@ -20,23 +20,46 @@ class Xchg(Instruction):
     def name(self):
         return 'xchg'
 
-    def reg1(self):
+
+class XchgRegReg(Xchg):
+    def regL(self):
         return 'ax'
 
-    def reg2(self):
+    def regR(self):
         return reg16[self.opcode & 0b111]
 
     def render(self, addr):
-        tokens = Instruction.render(self, addr)
+        tokens = Xchg.render(self, addr)
         tokens += asm(
-            ('reg', self.reg1()),
+            ('reg', self.regL()),
             ('opsep', ', '),
-            ('reg', self.reg2()),
+            ('reg', self.regR()),
         )
         return tokens
 
     def lift(self, il, addr):
         temp = LLIL_TEMP(il.temp_reg_count)
-        il.append(il.set_reg(2, temp, il.reg(2, self.reg1())))
-        il.append(il.set_reg(2, self.reg1(), il.reg(2, self.reg2())))
-        il.append(il.set_reg(2, self.reg2(), il.reg(2, temp)))
+        il.append(il.set_reg(2, temp, il.reg(2, self.regL())))
+        il.append(il.set_reg(2, self.regL(), il.reg(2, self.regR())))
+        il.append(il.set_reg(2, self.regR(), il.reg(2, temp)))
+
+
+class XchgRegRM(InstrHasModRegRM, InstrHasWidth, Xchg):
+    def regL(self):
+        return self._reg()
+
+    def render(self, addr):
+        tokens = Xchg.render(self, addr)
+        tokens += asm(
+            ('reg', self.regL()),
+            ('opsep', ', '),
+        )
+        tokens += self._render_reg_mem()
+        return tokens
+
+    def lift(self, il, addr):
+        w = self.width()
+        temp = LLIL_TEMP(il.temp_reg_count)
+        il.append(il.set_reg(w, temp, il.reg(w, self.regL())))
+        il.append(il.set_reg(w, self.regL(), self._lift_reg_mem(il)))
+        il.append(self._lift_reg_mem(il, store=il.reg(w, temp)))
